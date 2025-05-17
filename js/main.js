6c7472a775fd1193e5de160da9e26cdbf9dbc595
franco <francosantillan12@gmail.com>
2025-05-17 19:35:19 -0300

    Subida inicial del proyecto

---
+++ js/main.js
@@ -0,0 +1,46 @@
+//let  curso = " JavaScript-flex" 
+/* De esta manera cree la valiable "curso" de tipo "sting" y 
+            le asigné el nombre "javascript-flex"*/
+
+
+
+
+//let comision = 90600 
+/* Creé la variable "comision" de tipo "numerica" y le asigné un valor */
+
+
+
+//let tutor = false 
+/* Creé la variable "tutor" de tipo "booblean" y
+ le asigné que sea "falso"  */ 
+
+
+//let plataforma = "CoderHouse"
+
+//console.log("Plataforma: "+plataforma)
+
+//const academia = "CoderHouse"
+//ACÁ CREE UNA "CONSTANTE" CON EL NOMBRE "CODERHOUSE" 
+
+
+//let curso = prompt ("Ingrese la materia")
+//let comision = prompt ("Ingrese el numero de comision") 
+
+
+ 
+//console.log("Materia: "+curso+" Comision N "+comision+ "Academia:"+academia)
+//console.log ("Comision N"+ comision)
+
+//con "parseInt" todo lo que escribamos se transforma en tipo numerico
+ let numeroA = parseInt(prompt("ingrese el primer numero")) 
+ let numeroB = parseInt(prompt("ingrese el segundo numero"))
+
+
+ let resultado = numeroA + numeroB 
+ console.log(resultado)
+
+
+
+
+
+ 
