876a21873568549e3e20549320af052c9f08dc65
franco <francosantillan12@gmail.com>
2025-06-04 20:52:19 -0300

    Calculadora funcionando

--- js/main.js
+++ js/main.js
@@ -1,3 +1,104 @@
+//******* MI CALCULADORA********** */
+
+const pedirNumeros = () => {
+  let numeroA = parseInt(prompt("Ingrese el primer número"))
+  let numeroB = parseInt(prompt("Ingrese el segundo número"))
+  return [numeroA, numeroB];
+}
+
+const sumar = (numeroA, numeroB) => {
+  let resultado = numeroA + numeroB
+  alert (numeroA + "+" + numeroB + "=" + resultado)
+}
+
+const restar = (numeroA, numeroB) => {
+  let resultado = numeroA - numeroB
+  alert (numeroA + "-" + numeroB + "=" + resultado)
+}
+
+ const multiplicar = (numeroA, numeroB) => {
+  let resultado = numeroA * numeroB
+  alert (numeroA + "*" + numeroB + "=" + resultado)
+ }
+
+const dividir = (numeroA, numeroB) => {
+  let resultado = numeroA / numeroB
+  alert (numeroA + "/" + numeroB + "=" + resultado)
+}
+
+
+
+let menu = parseInt(prompt("elija una opcion: \n 1-sumar \n 2-restar \n 3-multiplicar \n 4-dividir \n 5- salir"))
+
+while(menu !== 5) { //que el numero que se le cargue a "menu" tiene que ser distinto que 5, de esta manera salgo
+  let numeros = pedirNumeros()
+  switch(menu){
+  case 1: 
+    sumar(numeros[0], numeros[1])
+    break
+  case 2:
+    restar(numeros[0], numeros[1])
+    break
+  case 3:
+    multiplicar(numeros[0], numeros[1])
+    break
+  case 4:
+    dividir(numeros[0], numeros[1])
+    break
+  default:
+    alert("opcion incorrecta")
+}
+
+ menu = parseInt(prompt("elija una opcion: \n 1-sumar \n 2-restar \n 3-multiplicar \n 4-dividir \n 5- salir"))
+}
+alert("gracias")
+ 
+
+
+
+
+
+
+
+
+
+
+// function sumar (){
+//   let numeroA = parseInt(prompt("ingrese el primer numero"))
+//   let numeroB = parseInt(prompt("ingrese el segundo numero"))
+//   let resultado = numeroA + numeroB
+//   alert(numeroA + "+" + numeroB + "=" + resultado )
+// }
+
+//  function restar (){
+//    let numeroA = parseInt(prompt("ingrese el primer numero"))
+//    let numeroB = parseInt(prompt("ingrese el segundo numero"))
+//    let resultado = numeroA - numeroB
+//    alert(numeroA + "-" + numeroB + "=" + resultado )
+// }
+
+// function multiplicar (){
+//   let numeroA = parseInt(prompt("ingrese el primer numero"))
+//   let numeroB = parseInt(prompt("ingrese el segundo numero"))
+//   let resultado = numeroA * numeroB
+//   alert(numeroA + "x" + numeroB + "=" + resultado )
+// }
+
+
+// function dividir (){
+//   let numeroA = parseInt(prompt("ingrese el primer numero"))
+//   let numeroB = parseInt(prompt("ingrese el segundo numero"))
+//   let resultado = numeroA / numeroB
+//   alert(numeroA + " / " + numeroB + "=" + resultado )
+// }
+
+// Función para pedir los dos números
+
+
+
+
+
+
 //let  curso = " JavaScript-flex" 
 /* De esta manera cree la valiable "curso" de tipo "sting" y 
             le asigné el nombre "javascript-flex"*/
@@ -32,15 +133,231 @@
 //console.log ("Comision N"+ comision)
 
 //con "parseInt" todo lo que escribamos se transforma en tipo numerico
- let numeroA = parseInt(prompt("ingrese el primer numero")) 
- let numeroB = parseInt(prompt("ingrese el segundo numero"))
+// let numeroA = parseInt(prompt("ingrese el primer numero")) 
+ //let numeroB = parseInt(prompt("ingrese el segundo numero"))
 
 
- let resultado = numeroA + numeroB 
- console.log(resultado)
+ //let resultado = numeroA + numeroB 
+ //console.log(resultado)
 
 
 
 
+ //let edad = 20
+ //let acceso = (edad => 18) && (edad <= 30)
+ //console.log (acceso)
 
- 
+ //let temperatura = 91
+ //let encender = (temperatura >= 80) && (temperatura <=93)
+ //console.log (encender)
+
+ //let edad = 20;
+ //let dinero = false
+
+ //if(edad >= 21 || dinero) {
+   // console.log("ingresá")
+ //} else {
+ //   console.log("no podes ingresar bro")
+ //} 
+
+
+//AND (&&) = se tienen que cumplir ambas condiciones para que el resultado sea true.
+
+//OR (||) = se tiene que cumplir al menos una de las condiciones
+
+
+
+
+/*
+let edad = null;
+let nombre = "Carlos";
+
+if (edad !== null && edad !== undefined) {
+  console.log(`Tienes ${edad} años`);
+} else {
+  if (nombre) {
+    console.log(`Bienvenido, ${nombre}`);
+  } else {
+    console.log("Información incompleta");
+  }
+}
+  
+En este ejemplo:
+
+Paso 1: Verificamos si edad tiene un valor válido.
+
+Paso 2: Si no, evaluamos si nombre está definido.
+
+Paso 3: Dependiendo de las condiciones, mostramos el mensaje correspondiente.
+
+*/
+
+//USUARIOS
+//const usuarios =  [
+  //  {nombre: "Ana", edad: 20, aceptoTerminos: false},
+    //{nombre: "Luis", edad:18,  aceptoTerminos: true},
+    //{nombre: "Carlos", edad: 18, aceptoTerminos: true},
+   // {nombre: "Maria", edad: 20, aceptoTerminos: true}
+//]
+
+//for (let i = 0; i < usuarios.length; i++){ // propiedad (.length) "le decis, va a iterar siempre que I sea menor a la cantidad de elementos que tiene el array de usuarios
+//    if(usuarios[i].edad >=18 && usuarios[i].aceptoTerminos){
+//        console.log(usuarios[i].nombre)
+//    }
+//} 
+
+// VALIDADOR DE ACCESO
+
+
+//let = color = prompt("color del semaforo (rojo, amarillo, verde)")
+
+
+
+//if (color === "rojo"){
+ // console.log("ALTO!")
+//} else if (color === ("amarillo"){
+//  console.log ("ATENCION!")
+//} else (color === "verde"){
+//  console.log("avance")
+//}
+
+
+
+//*********QUIZ DE TRES PREGUNTAS**************** */
+/*let jugarOtraVez = prompt ("¿Estas listo para este quiz de 3 preguntas? si/no").toLowerCase().trim();
+
+while (jugarOtraVez === "si"){
+
+let puntaje = 0
+let respuestaUsuario = prompt ("¿Quien salió campeón del mundo en el mundial de qatar 2022?\na) Alemania\nb) Francia\nc) Argentina");
+let respuestaUsuario2 = prompt ("¿Quien metió 3 goles en la final del mundo en qatar 2022?\na) Mercado \nb) Mbappe \nc) Messi ");
+let respuestaUsuario3 = prompt ("¿Quien patió el ultimo penal en la final en el mundial de qatar 2022? \na) Montiel \nb) Messi \nc) Tragliafico");
+
+  
+  if (respuestaUsuario === "c"){
+  puntaje++
+  console.log("Correcto, sumaste 1 punto!")
+} else  {
+  console.log ("Incorrecto")
+}
+
+
+if (respuestaUsuario2 === "b"){
+  puntaje++
+  console.log("Correcto, sumaste 1 punto")
+} else {
+  console.log ("Incorrecto, no sumaste puntos")
+}
+
+
+if (respuestaUsuario3 === "a"){
+  puntaje++
+  console.log("Correcto! sumaste 1 punto")
+}else {
+  console.log ("Incorrecto, no sumaste nada")
+}
+
+
+alert("Terminaste el quiz. Acertaste " + puntaje + " de 3 preguntas")
+jugarOtraVez = prompt ("¿Querés seguír jugando? si/no").toLowerCase().trim();
+}
+*/
+
+/*function saludar(nombre){
+  console.log("hola" + nombre + "!");
+}*/
+
+
+
+
+/*function numero (numeroPar){
+  if (numeroPar % 2 == 0){
+    console.log("es par")
+  }else{
+    console.log("es impar")
+  }
+}
+
+let  entrada = prompt("ingrese un numero");
+let numeroUsuario = Number(entrada);
+numero(numeroUsuario); */
+
+
+/*function edad (edad){
+  if (edad >= 18){
+  console.log("podes votar")
+  }else{
+  console.log("no podes votar")
+}
+}
+edad(21)//consola devuelte "podes votar"*/
+
+/*function valorDeLibros(preciodelibro){
+  const alquilerPorDia = 10
+  return preciodelibro + alquilerPorDia
+}
+let libro = valorDeLibros(10)
+let dias = alquilerPorDia + valorDeLibros
+
+console.log(`el valor del libro es: $${libro}`)
+*/
+
+
+//let nombre = prompt("Decime tu nombre") 
+//let edad = parseInt(prompt("Decime tu edad"))
+/*let signo = prompt("¿Que signo sos?")
+
+function saludar(nombre, edad, signo) {
+  console.log("Hola " + nombre)
+  console.log("Tu edad es " + edad)
+  console.log("tu signo es " + signo)
+
+  if (isNaN(edad)) {
+    console.log("La edad ingresada no es válida.")
+  } else if (edad >= 18) {
+    console.log("Sos mayor de edad.")
+  } else {
+    console.log("Sos menor de edad.")
+  } if (signo === "picis"){
+    console.log("sos picis!!!,como justin")
+  }else {
+    console.log("No sos como justin")
+  }
+}
+
+saludar(nombre, edad, signo)
+*/
+
+// let edadIngresada = parseInt(prompt("¿cuantos años tenes?"))
+// function verificarEdad (){
+//   let mayorDeEdad = edad >= 18
+//   alert ()
+// }
+
+
+
+
+// SUMA 
+//let primerNumero = parseInt(prompt("ingrese el primer numero"))
+//let segundoNumero = parseInt(prompt("ingrese el segundo numero"))
+
+//function sumar (numeroA, numeroB){
+//  let resultado = numeroA + numeroB
+//  return resultado
+//}
+
+//console.log(sumar(primerNumero, segundoNumero))
+//***************HASTA ACÁ********* */
+
+//********* SUMA CON FUNCION FLECHA!!**** 
+
+//const calculadoraDel10 = primerNumero => primerNumero*10
+//  console.log (calculadoreaDel10(10))
+
+
+
+
+//********************* */
+
+
+
